b88126790594015b93a72fccb7e251f6532defac
yadavdesh <yadavdesh@gmail.com>
2016-08-10 17:50:34 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,11 @@
+$(document).ready(function(){
+  $('body').append('<div></div>');
+  $('div').css('background-color', 'blue');
+  $('div').height('700px');
+  $('div').width('700px');
+  $('div').css('margin', 'auto');
+  
+   
+  
+});
 
